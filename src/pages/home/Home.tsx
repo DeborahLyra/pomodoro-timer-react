7dbffc20ae1245ec317cf0a8ead244bd1243b492
Deborah Lyra <deborahp.lyra@gmail.com>
2024-05-15 20:50:23 -0300

    added readme and validation form

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,7 @@
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
 import {
   CountDownContainer,
   FormContainer,
@@ -9,16 +11,29 @@ import {
   StartCountdownButton,
   TaskInput,
 } from './styles'
-import { useState } from 'react'
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod.number().min(5).max(60)
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema> //para refenciar uma variável JS em TS, precisa ser tipado
 
 export function Home() {
+  
 
-  const { handleSubmit, register, watch } = useForm()
+  const { handleSubmit, register, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0
+    }
+  })
 
   const isSubmitDisabled = watch('task') //observa o input registrado como task, para saber o momento que não está mais vazio
 
-  const handleCreateNewCycle = (data: any) => {
-
+  const handleCreateNewCycle = (data: NewCycleFormData) => {
+    reset()
   }
 
   return (
@@ -60,7 +75,7 @@ export function Home() {
           <span>0</span>
         </CountDownContainer>
 
-        <StartCountdownButton disabled = {!isSubmitDisabled} type="submit">
+        <StartCountdownButton disabled={!isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
