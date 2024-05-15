f47cbc377e01c14586530892480b5772b68c5693
Deborah Lyra <deborahp.lyra@gmail.com>
2024-05-15 15:30:59 -0300

    ?

--- src/@types/styles.d.ts
+++
@@ -1,9 +0,0 @@
-// código apenas de definição de tipos de TS - só para tipagem
-
-import { defaultThemes } from '../styles/default'
-
-type ThemeType = typeof defaultThemes
-
-declare module 'styled-components' {
-  export interface DefaultThemes extends ThemeType {}
-}
